3854ff702f0547d6395ff58eba11c410e43eebec
thanhdo3764 <thanh.do3764@gmail.com>
2024-11-01 13:46:31 -0700

    Implement PageRank into the search engine

--- anime_search_engine.py
+++ anime_search_engine.py
@@ -1,34 +1,42 @@
 from whoosh.index import create_in, open_dir, exists_in
 from whoosh.fields import *
 from whoosh.analysis import StemmingAnalyzer
-from bs4 import BeautifulSoup
 from whoosh.qparser import QueryParser, OrGroup, AndGroup
+from whoosh import scoring
+
+from bs4 import BeautifulSoup
+
 import os, pickle
 
 class SearchEngine(object):
 
 	def __init__(self):
 		self.schema = Schema(title=TEXT(stored=True), url = ID(stored=True), content=TEXT(analyzer=StemmingAnalyzer()))
-		self.ix = None
-		if not os.path.exists("./indexdir"): os.mkdir("./indexdir")
-		if not exists_in("./indexdir"): self.create_index()
-		else: self.ix = open_dir("./indexdir")
+		self.ix = self.__get_indexer("./indexdir")
 		self.limit = 10 # Number of results displayed
 		self.conj = True
+		self.size = self.ix.doc_count()
+		self.page_rank = self.__unpickle("./page_rank.dat")
+		assert self.size == len(self.page_rank), "the index and page rank don't match"
 
+	# Returns an existing or new indexer 
+	def __get_indexer(self, path):
+		if not os.path.exists(path): os.mkdir(path)
+		if not exists_in(path): return self.__create_index(path)
+		else: return open_dir(path)
 
 	# Get url map from path file
-	def get_url_map(self, path):
+	def __unpickle(self, path):
 		data = None;
 		with open(path,"rb") as f:
 			data = pickle.load(f)
 		return data
 
-	# Initializes self.ix with a Whoosh Index
-	def create_index(self):
-		self.ix = create_in("indexdir", self.schema)
-		writer = self.ix.writer(limitmb=1024, procs=4, multisegment=True)
-		urls = self.get_url_map("./sample/url_map.dat")
+	# Returns a new indexer with a Whoosh Index
+	def __create_index(self, path):
+		ix = create_in(path, self.schema)
+		writer = ix.writer(limitmb=1024, procs=4, multisegment=True)
+		urls = self.__unpickle("./sample/url_map.dat")
 		_title = ""
 		_url = ""
 		_content = ""
@@ -47,27 +55,38 @@ class SearchEngine(object):
 			writer.add_document(title=_title, url=u, content=_content)
 			count += 1
 		writer.commit()
+		return ix
 
 	# Print the search results
-	def print_result(self, results_obj):
+	def __print_result(self, results_obj):
+		print(results_obj.top_n)
 		print(f"--------------------\n{len(results_obj)} RESULTS")
 		if len(results_obj) == 0: print("No results found")
 		for result in results_obj: print(f"{result["title"]}\n\t\033[94m{result["url"]}\033[0m\n")
 		print("--------------------")
 
+	# Combines page rank and bm25 to be used with scoring.FunctionWeighting
+	def __custom_scorer(self, searcher, fieldname, text, matcher):
+		html = list(searcher.documents())[matcher.id()]["url"]
+		pr = self.page_rank[html]
+		bm25 = scoring.BM25F().scorer(searcher, fieldname, text).score(matcher)
+		a = 0.5
+		b = 0.5
+		return a*pr + b*bm25
+
 	# Perform search for a query in the index and print the result
 	def query_search(self, query_string):
 		print(f"SEARCHING: {query_string}")
-		with self.ix.searcher() as searcher:
+		with self.ix.searcher(weighting=scoring.FunctionWeighting(self.__custom_scorer)) as searcher:
 			# Construct query based on self.conj
 			if self.conj: query = QueryParser("content", self.ix.schema, group=AndGroup).parse(query_string)
 			else: query = QueryParser("content", self.ix.schema, group=OrGroup).parse(query_string)
 			results = searcher.search(query, limit = self.limit)
-			self.print_result(results)
+			self.__print_result(results)
 		
 
 def main():
-	string = "animes"
+	string = "anime"
 	mySearchEngine = SearchEngine()
 	mySearchEngine.query_search(string)
 	
--- page_rank.py
+++ page_rank.py
@@ -1,9 +1,9 @@
 import numpy as np
-import csv
+import csv, pickle
 
 # Returns a page rank vector for a given transition matrix, number
 # of links, damping factor, and epsilon threshold
-def page_rank(L, n, d=0.85, epsilon = 0.0001):
+def __page_rank(L, n, d=0.85, epsilon = 0.0001):
 	p_new = np.full(n, 1/n)
 	while True:
 		lp = np.dot(L, p_new)
@@ -12,7 +12,7 @@ def page_rank(L, n, d=0.85, epsilon = 0.0001):
 		if not np.any(abs(p_old-p_new) >= epsilon): return p_new
 	
 # Returns a transition matrix of an adjacency matrix
-def adjacency_to_transition_matrix(adjMatrix):
+def __adjacency_to_transition_matrix(adjMatrix):
 	row_count = 0
 	for row in adjMatrix:
 		one_count = np.count_nonzero(row)
@@ -21,7 +21,7 @@ def adjacency_to_transition_matrix(adjMatrix):
 	return np.transpose(adjMatrix)
 
 # Reads a given csv file and returns a matrix
-def csv_to_matrix(path):
+def __csv_to_matrix(path):
 	matrix = []
 	with open(path, "r" ,newline = '') as file:
 		csv_file = csv.reader(file)
@@ -29,3 +29,20 @@ def csv_to_matrix(path):
 		for lines in csv_file:
 			matrix.append(lines[1:])
 	return np.array(matrix,dtype=float)
+
+# Returns a dictionary mapping url to PageRank score
+def __pickle_page_rank(csv_path, d=0.85, epsilon = 0.0001):
+	url_to_pr = {}
+	url_array = []
+	with open(csv_path, "r" ,newline = '') as file:
+		csv_file = csv.reader(file)
+		url_array = csv_file.__next__()[1:]
+	matrix = __csv_to_matrix(csv_path)
+	matrix = __adjacency_to_transition_matrix(matrix)
+	page_rank = __page_rank(matrix, matrix.shape[0], d, epsilon)
+	for i in range(matrix.shape[0]):
+		url_to_pr[url_array[i]] = float(page_rank[i])
+	with open("page_rank.dat","wb") as f:
+		pickle.dump(url_to_pr,f)
+
+__pickle_page_rank("./sample/adjacency_matrix.csv")
