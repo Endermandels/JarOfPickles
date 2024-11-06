d69858c729f3901a03eb2ad690ae5d7ba758f17e
thanhdo3764 <thanh.do3764@gmail.com>
2024-11-05 17:37:58 -0800

    Switch hard coded paths with variables

--- anime_search_engine.py
+++ anime_search_engine.py
@@ -10,20 +10,26 @@ import os, pickle
 
 class SearchEngine(object):
 
-	def __init__(self):
+	def __init__(self, index_dir = "./indexdir", page_rank_file = "./page_rank.dat", url_map_file = "./sample/url_map.dat", docs_raw_dir = "./sample/_docs_raw/", docs_cleaned_dir = "./sample/_docs_cleaned/"):
+		self.index_dir = index_dir
+		self.page_rank_file = page_rank_file
+		self.url_map_file = url_map_file
+		self.docs_raw_dir = docs_raw_dir
+		self.docs_cleaned_dir = docs_cleaned_dir
+
 		self.schema = Schema(title=TEXT(stored=True), url = ID(stored=True), content=TEXT(analyzer=StemmingAnalyzer()))
-		self.ix = self.__get_indexer("./indexdir")
+		self.ix = self.__get_indexer()
 		self.limit = 10 # Number of results displayed
 		self.conj = True
 		self.size = self.ix.doc_count()
-		self.page_rank = self.__unpickle("./page_rank.dat")
+		self.page_rank = self.__unpickle(page_rank_file)
 		assert self.size == len(self.page_rank), "the index and page rank don't match"
 
 	# Returns an existing or new indexer 
-	def __get_indexer(self, path):
-		if not os.path.exists(path): os.mkdir(path)
-		if not exists_in(path): return self.__create_index(path)
-		else: return open_dir(path)
+	def __get_indexer(self):
+		if not os.path.exists(self.index_dir): os.mkdir(self.index_dir)
+		if not exists_in(self.index_dir): return self.__create_index(self.index_dir)
+		else: return open_dir(self.index_dir)
 
 	# Get url map from path file
 	def __unpickle(self, path):
@@ -33,23 +39,23 @@ class SearchEngine(object):
 		return data
 
 	# Returns a new indexer with a Whoosh Index
-	def __create_index(self, path):
-		ix = create_in(path, self.schema)
+	def __create_index(self):
+		ix = create_in(self.index_dir, self.schema)
 		writer = ix.writer(limitmb=1024, procs=4, multisegment=True)
-		urls = self.__unpickle("./sample/url_map.dat")
+		urls = self.__unpickle(self.url_map_file)
 		_title = ""
 		_url = ""
 		_content = ""
-		htmlParser = None;
+		htmlParser = None
 		count = 1
 		# For each url mapped to a file name
 		for u in urls:
 			file_name = urls[u]
 			# Get the title for the file name
-			with open("./sample/_docs_raw/"+file_name, "r") as html:
+			with open(self.docs_raw_dir+file_name, "r") as html:
 				_title = BeautifulSoup(html.read(), "lxml").title.string.strip()
 			# Get the text content for the file name
-			with open("./sample/_docs_cleaned/"+file_name, "r") as text:
+			with open(self.docs_cleaned_dir+file_name, "r") as text:
 				_content = text.read()
 			print(f"({count}) Indexing {file_name}")
 			writer.add_document(title=_title, url=u, content=_content)
@@ -86,7 +92,7 @@ class SearchEngine(object):
 		
 
 def main():
-	string = "anime"
+	string = "tokyo"
 	mySearchEngine = SearchEngine()
 	mySearchEngine.query_search(string)
 	
