13ddc1e512ac19094ebb595586d1040d11dcf2b7
Endermandels <elijahdelavar@gmail.com>
2024-11-12 10:40:49 -0800

    Show live results from search engine results

--- front_end/app.py
+++ front_end/app.py
@@ -1,4 +1,17 @@
+import os
+import sys
+import inspect
+import signal
+
+# Move CWD to parent dir to have access to search_engine
+# Code from https://stackoverflow.com/questions/714063/importing-modules-from-parent-folder
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+
 from flask import Flask, render_template, request
+from search_engine.anime_search_engine import SearchEngine
+
 
 app = Flask(__name__)
 
@@ -6,34 +19,30 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# Code by Pretty Printed on YouTube: https://youtu.be/PWEl1ysbPAY?si=eKrzQqsts-G-TvkK
-
+# Inspired by Pretty Printed on YouTube: https://youtu.be/PWEl1ysbPAY?si=eKrzQqsts-G-TvkK
 @app.route('/search')
 def search():
     q = request.args.get('q')
     print(q)
     
-    # TODO: DELETE
-    docs = [
-        ['hello', 'world', 'it', 'is', 'cool']
-        , ['hello', 'world', 'it', 'is', 'nice']
-        , ['hello', 'world', 'it', 'is', 'awesome']
-        , ['hello', 'world', 'it', 'is', 'cheese']
-    ]
-    
     results = []
     
     if q:
-        # User has a query
-        # Search for matching docs
-        # TODO: Search for matches over corpus titles
-        for doc in docs:
-            for word in doc:
-                if q in word:
-                    results.append(doc)
-                    break
-        
+        dir = 'search_engine'
+        mySearchEngine = SearchEngine(
+            index_dir=f'../{dir}/indexdir/'
+            , page_rank_file=f'../{dir}/page_rank.dat'
+            , url_map_file=f'../{dir}/sample/url_map.dat'
+            , docs_raw_dir=f'../{dir}/sample/_docs_raw/'
+            , docs_cleaned_dir=f'../{dir}/sample/_docs_cleaned/')
+        mySearchEngine.submit_query(q)
+        results = mySearchEngine.return_page(1)['docs']
+        mySearchEngine.close_searcher()
+       
     return render_template("search_results.html", results=results)
+    
+def start_app():
+    app.run(debug=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    start_app()
