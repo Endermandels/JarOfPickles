91f9d4d827981bd66d6777c21aaa642fd655b639
thanhdo3764 <thanh.do3764@gmail.com>
2024-11-20 15:11:18 -0800

    Optimize PageRank

--- search_engine/page_rank.py
+++ search_engine/page_rank.py
@@ -6,18 +6,18 @@ import csv, pickle
 def __page_rank(L, n, d=0.85, epsilon = 0.0001):
 	p_new = np.full(n, 1/n)
 	while True:
-		lp = np.dot(L, p_new)
 		p_old = p_new
-		p_new = d*lp + ((1-d)/n)
+		p_new = np.dot(L, p_new)
 		if not np.any(abs(p_old-p_new) >= epsilon): return p_new
 	
 # Returns a transition matrix of an adjacency matrix
-def __adjacency_to_transition_matrix(adjMatrix):
+def __adjacency_to_transition_matrix(adjMatrix, n, d=0.85):
 	row_count = 0
 	for row in adjMatrix:
-		one_count = np.count_nonzero(row)
-		np.copyto(adjMatrix[row_count], row/one_count)
+		row_total = np.sum(row)
+		np.copyto(adjMatrix[row_count], row/row_total)
 		row_count += 1
+	adjMatrix = d*adjMatrix + ((1-d)/n)
 	return np.transpose(adjMatrix)
 
 # Reads a given csv file and returns a matrix
@@ -38,7 +38,7 @@ def __pickle_page_rank(csv_path, d=0.85, epsilon = 0.0001):
 		csv_file = csv.reader(file)
 		url_array = csv_file.__next__()[1:]
 	matrix = __csv_to_matrix(csv_path)
-	matrix = __adjacency_to_transition_matrix(matrix)
+	matrix = __adjacency_to_transition_matrix(matrix, matrix.shape[0], d)
 	page_rank = __page_rank(matrix, matrix.shape[0], d, epsilon)
 	for i in range(matrix.shape[0]):
 		url_to_pr[url_array[i]] = float(page_rank[i])
@@ -47,4 +47,4 @@ def __pickle_page_rank(csv_path, d=0.85, epsilon = 0.0001):
 
 	print(url_to_pr)
 
-__pickle_page_rank("./og_sample/adjacency_matrix.csv")
+__pickle_page_rank("./sample/adjacency_matrix.csv")
