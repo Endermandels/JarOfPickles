87bb4c6a6b979c8ac202987b08192d42d2fe6fc8
thanhdo3764 <thanh.do3764@gmail.com>
2024-11-20 22:47:03 -0800

    Further optimize page_rank.py to work on large adjacency matrix DataFrames

--- search_engine/page_rank.py
+++ search_engine/page_rank.py
@@ -1,50 +1,42 @@
 import numpy as np
-import csv, pickle
+import pickle
+import pandas as pd
+from scipy import sparse
+import gc
 
-# Returns a page rank vector for a given transition matrix, number
-# of links, damping factor, and epsilon threshold
+# Returns a page rank vector for a given SciPy transition matrix
+# (no added leap probability),number of links, damping factor, and epsilon threshold
 def __page_rank(L, n, d=0.85, epsilon = 0.0001):
 	p_new = np.full(n, 1/n)
 	while True:
 		p_old = p_new
-		p_new = np.dot(L, p_new)
+		p_new = L.dot(p_new) + ((1-d)/n)
 		if not np.any(abs(p_old-p_new) >= epsilon): return p_new
-	
-# Returns a transition matrix of an adjacency matrix
-def __adjacency_to_transition_matrix(adjMatrix, n, d=0.85):
-	row_count = 0
-	for row in adjMatrix:
-		row_total = np.sum(row)
-		np.copyto(adjMatrix[row_count], row/row_total)
-		row_count += 1
-	adjMatrix = d*adjMatrix + ((1-d)/n)
-	return np.transpose(adjMatrix)
 
-# Reads a given csv file and returns a matrix
-def __csv_to_matrix(path):
-	matrix = []
-	with open(path, "r" ,newline = '') as file:
-		csv_file = csv.reader(file)
-		csv_file.__next__()
-		for lines in csv_file:
-			matrix.append(lines[1:])
-	return np.array(matrix,dtype=float)
+# Returns a SciPy transition matrix of a SciPy adjacency matrix
+def __adjacency_to_transition_matrix(adjMatrix, d=0.85):
+	s = adjMatrix.sum(axis=1)
+	return (d*(adjMatrix/s)).T
 
-# Returns a dictionary mapping url to PageRank score
-def __pickle_page_rank(csv_path, d=0.85, epsilon = 0.0001):
+# Pickles a dictionary mapping url to PageRank score for a Pandas DataFrame
+def __pickle_page_rank(df_path, d=0.85, epsilon = 0.0001):
+	df = pd.read_pickle(df_path)
 	url_to_pr = {}
-	url_array = []
-	with open(csv_path, "r" ,newline = '') as file:
-		csv_file = csv.reader(file)
-		url_array = csv_file.__next__()[1:]
-	matrix = __csv_to_matrix(csv_path)
-	matrix = __adjacency_to_transition_matrix(matrix, matrix.shape[0], d)
+	url_array = list(df.columns)
+	matrix = sparse.csr_matrix(df.values)
+
+	df = None # Python can garbage collect the DataFrame
+
+	matrix = __adjacency_to_transition_matrix(matrix, d)
+
 	page_rank = __page_rank(matrix, matrix.shape[0], d, epsilon)
+
 	for i in range(matrix.shape[0]):
 		url_to_pr[url_array[i]] = float(page_rank[i])
+
 	with open("page_rank.dat","wb") as f:
 		pickle.dump(url_to_pr,f)
 
-	print(url_to_pr)
+__pickle_page_rank("./sample/adjacency_matrix.dat")
 
-__pickle_page_rank("./sample/adjacency_matrix.csv")
+
